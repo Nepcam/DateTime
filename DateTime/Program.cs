46d7b8ac72a76df6db59a67934747199f099c54d
Nepcam <cameron.nepe@gmail.com>
2020-03-24 18:10:57 +1300

    date and time formats

--- DateTime/Program.cs
+++ DateTime/Program.cs
@@ -11,8 +11,26 @@ namespace DateTimeExercise
         static void Main(string[] args)
         {
             DateTime myDate = DateTime.Now;
-            string formattedDate = string.Format("Date is {0:yyyy MMMM dddd}",myDate); // "{0}" = this is a placeholder 
+            string formattedDate = "";
+
+            //First Format: eg. 24-03-2020 18:01:00
+            formattedDate = string.Format("{0:dd-MM-yyyy HH:mm:ss}",myDate);
             Console.WriteLine(formattedDate);
+
+            //Second Format: eg. Tuesday of month March 2020
+            formattedDate = string.Format("{0:dddd 'of month' MMMM 'year' yyyy}",myDate);
+            Console.WriteLine(formattedDate);
+
+            /* Third Format
+             * Day Tuesday
+             * Month March
+             * Year 2020
+             */
+            formattedDate = string.Format("{0:'Day' dddd \n 'Month' MMMM \n 'Year' yyyy}", myDate);
+            Console.WriteLine(formattedDate);
+
+            //string formattedDate = string.Format("Date is {0:yyyy MMMM dddd}",myDate); // "{0}" = this is a placeholder 
+            //Console.WriteLine(formattedDate);
         }
     }
 }
